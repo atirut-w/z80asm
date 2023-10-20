f2792c3a334189a823fcc899654631be5acb8411
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-10-20 14:34:48 +0700

    Grammar stubs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use clap::Parser;
+use clap::Parser as ClapParser;
 use std::path::PathBuf;
+use pest_derive::Parser as PestParser;
 
-#[derive(Parser)]
+#[derive(PestParser)]
+#[grammar = "grammar.pest"]
+struct ASMParser;
+
+#[derive(ClapParser)]
 struct Args {
     /// Input file
     input: PathBuf,
