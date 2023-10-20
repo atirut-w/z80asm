368ade2c88a7a498c280324fbfb0476a3152c5a9
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-10-20 09:44:39 +0700

    Implement argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+struct Args {
+    /// Input file
+    input: PathBuf,
+
+    /// Output file
+    #[clap(short, long)]
+    output: Option<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
