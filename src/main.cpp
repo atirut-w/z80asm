4b2cfc7acda8554f7c0df9a7d267386bbd26ddae
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-10-29 08:27:55 +0700

    Implement arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,30 @@
-int main()
+#include <argparse/argparse.hpp>
+#include <iostream>
+
+using namespace argparse;
+using namespace std;
+
+int main(int argc, char *argv[])
 {
+    ArgumentParser program = ArgumentParser("z80asm");
+
+    program.add_argument("input")
+        .help("input file");
+    
+    program.add_argument("-o", "--output")
+        .help("output file")
+        .default_value("a.out");
+
+    try
+    {
+        program.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error &err)
+    {
+        std::cout << err.what() << std::endl;
+        std::cout << program;
+        exit(1);
+    }
+
     return 0;
 }
