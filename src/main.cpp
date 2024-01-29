b4c8f142ffabd5cca4ad544f79e10c9d2cc3e111
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-01-29 13:08:33 +0700

    Add `argparse`

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,31 @@
-int main()
+#include <argparse/argparse.hpp>
+#include <memory>
+#include <iostream>
+
+auto parse_arguments(int argc, char **argv)
 {
+    auto parser = std::make_shared<argparse::ArgumentParser>("z80asm");
+
+    parser->add_argument("input")
+        .help("input file");
+
+    try
+    {
+        parser->parse_args(argc, argv);
+    }
+    catch (const std::runtime_error &err)
+    {
+        std::cout << err.what() << std::endl;
+        std::cout << *parser << std::endl;
+        exit(0);
+    }
+
+    return parser;
+}
+
+int main(int argc, char **argv)
+{
+    auto args = parse_arguments(argc, argv);
+
     return 0;
 }
