0fd06c5c7189ec55deb2ce050e783ce737deead5
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-01-29 21:53:46 +0700

    WIP IR class
    TODO: Finish the implementations

--- include/input_reader.hpp
+++ include/input_reader.hpp
@@ -1,1 +1,20 @@
 #pragma once
+#include <string>
+#include <istream>
+
+class InputReader
+{
+private:
+    std::string input;
+    int pos = 0;
+    int line = 1;
+    int column = 1;
+public:
+    InputReader(std::istream &stream);
+    
+    char peek();
+    char peek(int offset);
+    char consume();
+    void consume(int offset);
+    bool eof();
+};
--- src/input_reader.cpp
+++ src/input_reader.cpp
@@ -1,1 +1,9 @@
 #include <input_reader.hpp>
+#include <sstream>
+
+InputReader::InputReader(std::istream &stream)
+{
+    std::stringstream buffer;
+    buffer << stream.rdbuf();
+    input = buffer.str();
+}
