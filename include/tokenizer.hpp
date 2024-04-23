1dfc7234221501f690c38e40f31e701b0607eb56
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-04-23 13:50:08 +0700

    WIP tokenization logic

--- include/tokenizer.hpp
+++ include/tokenizer.hpp
@@ -1,18 +1,32 @@
 #pragma once
 #include <input_reader.hpp>
 #include <vector>
+#include <string>
 
 struct Token
 {
+    enum Type
+    {
+        TYPE_UNKNOWN,
+        TYPE_IDENT,
+        TYPE_WHITESPACE,
+        TYPE_COLON,
+        TYPE_COMMA,
+    } type = TYPE_UNKNOWN;
+    std::string value;
 };
 
 class Tokenizer
 {
 private:
     InputReader reader;
+    Token current_token;
+
+    void flush_token();
 
 public:
     std::vector<Token> tokens;
 
     Tokenizer(std::shared_ptr<std::istream> stream);
+    void tokenize();
 };
--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -5,3 +5,21 @@ using namespace std;
 Tokenizer::Tokenizer(std::shared_ptr<std::istream> stream) : reader(stream)
 {
 }
+
+void Tokenizer::flush_token()
+{
+    if (current_token.value.length() > 0)
+    {
+        tokens.push_back(current_token);
+        current_token.type = Token::TYPE_UNKNOWN;
+        current_token.value = "";
+    }
+}
+
+void Tokenizer::tokenize()
+{
+    while (!reader.eof())
+    {
+        reader.consume();
+    }
+}
